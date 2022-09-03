fe97f94cf69bc705fb00c91fabdd0eb161de78f4
sahneisttoll <sahneisttoll@gmail.com>
2022-09-04 01:52:58 +0200

    teleport yourself > teleport to camera

--- Online/teleport-to-camera.lua
+++ Online/teleport-to-camera.lua
@@ -4,4 +4,4 @@ local TeleportAngle = ac.getCameraForward()
 physics.setCarVelocity(0, vec3(0, 0, 0))
 physics.setCarPosition(0, teleportPoint, -TeleportAngle)
 end
-ui.registerOnlineExtra(ui.Icons.Bluetooth, 'Teleport yourself', nil, nil, Teleportar, ui.OnlineExtraFlags.Tool)
+ui.registerOnlineExtra(ui.Icons.Bluetooth, 'Teleport To Camera', nil, nil, Teleportar, ui.OnlineExtraFlags.Tool)

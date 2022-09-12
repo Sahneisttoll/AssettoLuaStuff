c25177bd6adb0798b7659e13bbc96d8b6af7856f
sahneisttoll <sahneisttoll@gmail.com>
2022-09-12 22:34:12 +0200

    you can see how fast they are going now

--- Online/teleport-to-car.lua
+++ Online/teleport-to-car.lua
@@ -1,15 +1,19 @@
 --[[slight edit of https://github.com/razaqq/AssettoServer/blob/TeleportPlugin/TeleportPlugin/lua/teleport-to-car.lua
 for normal users to teleport no need to be admin]]
+
 local sim = ac.getSim()
+
 local selectedCar = nil ---@type ac.StateCar
+
 local function TeleportHUD()
 	ui.text("Select car to teleport to:")
 	ui.childWindow("##drivers", vec2(ui.availableSpaceX(), 120), function()
 		for i = 1, sim.carsCount - 1 do
 			local car = ac.getCar(i)
+			local speed = math.floor(ac.getCar(i).speedKmh)
 			local driverName = ac.getDriverName(i)
 			if car.isConnected and not car.isAIControlled and not string.find(driverName, "Traffic") then
-				if ui.selectable(driverName, selectedCar == car) then
+				if ui.selectable(driverName .. " " .. speed .. " kmh", selectedCar == car) then
 					selectedCar = car
 				end
 				if ui.button("Teleport") and selectedCar then

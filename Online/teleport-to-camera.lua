56e5e8e054d6ba1d6b833cc1293f2a195ac741ad
sahneisttoll <sahneisttoll@gmail.com>
2022-09-12 22:34:02 +0200

    velocity last instead of first

--- Online/teleport-to-camera.lua
+++ Online/teleport-to-camera.lua
@@ -1,7 +1,7 @@
 local function Teleportar()
 	local teleportPoint = ac.getCameraPosition()
 	local TeleportAngle = ac.getCameraForward()
-	physics.setCarVelocity(0, vec3(0, 0, 0))
 	physics.setCarPosition(0, teleportPoint, -TeleportAngle)
+    physics.setCarVelocity(0, vec3(0, 0, 0))
 end
 ui.registerOnlineExtra(ui.Icons.Bluetooth, "Teleport To Camera", nil, nil, Teleportar, ui.OnlineExtraFlags.Tool)

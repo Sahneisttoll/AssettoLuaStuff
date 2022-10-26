7843341008767288217b180e1cff36f7f2716f3e
sahneisttoll <sahneisttoll@gmail.com>
2022-10-26 14:15:31 +0200

    better ui le text

--- Online/teleport-to-camera.lua
+++ Online/teleport-to-camera.lua
@@ -4,4 +4,4 @@ local function Teleportar()
 	physics.setCarPosition(0, teleportPoint, -TeleportAngle)
     physics.setCarVelocity(0, vec3(0, 0, 0))
 end
-ui.registerOnlineExtra(ui.Icons.Bluetooth, "Teleport To Camera", nil, nil, Teleportar, ui.OnlineExtraFlags.Tool)
+ui.registerOnlineExtra(ui.Icons.Bluetooth, "TP to Cam", nil, nil, Teleportar, ui.OnlineExtraFlags.Tool)

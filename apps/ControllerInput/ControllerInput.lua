9628420d170f978d081b6a81cc08d7daa0257329
sahneisttoll <sahneisttoll@gmail.com>
2023-04-13 17:58:31 +0200

    REAL TRAP SHIT, redone it with the least amount of controller guessing, if you dont something, edit the lua :trollface:

--- apps/ControllerInput/ControllerInput.lua
+++ apps/ControllerInput/ControllerInput.lua
@@ -4,23 +4,16 @@
 local FirstRun = true
 local sim = ac.getSim()
 local car = ac.getCar(0)
-local GameSize = vec2(sim.windowWidth, sim.windowHeight)
 local AppPos = vec2()
 local AppSize = vec2()
 local Scale = 0
 
-local Controller = 0
+local Controller = -1
 local LeftStick = vec2()
 local RightStick= vec2()
 local Triggers 	= vec2()
 local LeftStickMiddle 	= vec2()
 local RightStickMiddle 	= vec2()
-
-local dualsense = ac.getDualSense(4)
-if dualsense ~= nil then
-	local Touch1 = dualsense.touches[0]
-	local Touch2 = dualsense.touches[1]
-end
 
 --#region [Luts]
 local TriggerStart = math.rad(179)
@@ -44,10 +37,39 @@ function GetControllerIndex()
 	FirstRun = false
 end
 
+local DPAD_Icon = ac.dirname() .. "\\DPAD.png"
+local DPAD_UP_ON = ui.atlasIconID(DPAD_Icon, vec2(0.5, 0)	, vec2(1, 0.25))
+local DPAD_UP_OFF = ui.atlasIconID(DPAD_Icon, vec2(0, 0)	, vec2(0.5, 0.25))
+
+local DPAD_RIGHT_ON = ui.atlasIconID(DPAD_Icon, vec2(0.5, 0.25)	, vec2(1, 0.5))
+local DPAD_RIGHT_OFF = ui.atlasIconID(DPAD_Icon, vec2(0, 0.25)	, vec2(0.5, 0.5))
+
+local DPAD_DOWN_ON = ui.atlasIconID(DPAD_Icon, vec2(0.5, 0.5)	, vec2(1, 0.75))
+local DPAD_DOWN_OFF = ui.atlasIconID(DPAD_Icon, vec2(0, 0.5)	, vec2(0.5, 0.75))
+
+local DPAD_LEFT_ON = ui.atlasIconID(DPAD_Icon, vec2(0.5, 0.75), vec2(1, 1))
+local DPAD_LEFT_OFF = ui.atlasIconID(DPAD_Icon, vec2(0, 0.75), vec2(0.5, 1))
+
+
+local RightOne = ac.dirname() .. "\\brah.png"
+local ShiftUp_ON = ui.atlasIconID(RightOne, vec2(0.5, 0)	, vec2(1, 0.25))
+local ShiftUp_OFF = ui.atlasIconID(RightOne, vec2(0, 0)	, vec2(0.5, 0.25))
+
+local ShiftDown_ON = ui.atlasIconID(RightOne, vec2(0.5, 0.25)	, vec2(1, 0.5))
+local ShiftDown_OFF = ui.atlasIconID(RightOne, vec2(0, 0.25)	, vec2(0.5, 0.5))
+
+local HandBreak_ON = ui.atlasIconID(RightOne, vec2(0.5, 0.5)	, vec2(1, 0.75))
+local HandBreak_OFF = ui.atlasIconID(RightOne, vec2(0, 0.5)	, vec2(0.5, 0.75))
+
+local Clutch_ON = ui.atlasIconID(RightOne, vec2(0.5, 0.75), vec2(1, 1))
+local Clutch_OFF = ui.atlasIconID(RightOne, vec2(0, 0.75), vec2(0.5, 1))
+
+
+
 function script.update()
 	if FirstRun == true then
 		GetControllerIndex()
-		ac.debug("Cont",Controller)
+		ac.debug("What Input am i?", Controller < 4 and "Xinput" or Controller >= 4 and "DINPUT")
 	end
 end
 --real steer -1 to 1   RealSteering = car.steer/car.steerLock
@@ -99,9 +121,26 @@ function ControllerInput()
 	RealSteering:scale(100):scale(Scale)
 	RightStickMiddle:set(vec2(384,128)):scale(Scale)
 	RightStick	:scale(100):scale(Scale)
+
+	DPAD_color		= rgbm(1,1,1,0.8)
+	Dpad_pos_up		= LeftStickMiddle + vec2(-32,-96) * Scale
+	Dpad_pos_right	= LeftStickMiddle + vec2(32,-32) * Scale
+	Dpad_pos_down	= LeftStickMiddle + vec2(-32,32) * Scale
+	Dpad_pos_left	= LeftStickMiddle + vec2(-96,-32) * Scale
+	DPAD_Size		= 64 * Scale
+
+	other_color		= rgbm(1,1,1,0.8)
+	other_pos_up	= RightStickMiddle + vec2(-32,-96) * Scale
+	other_pos_right	= RightStickMiddle + vec2(32,-32) * Scale
+	other_pos_down	= RightStickMiddle + vec2(-32,32) * Scale
+	other_pos_left	= RightStickMiddle + vec2(-96,-32) * Scale
+	other_Size		= 64 * Scale
 	--#endregion
 
 	ui.transparentWindow("##ATransparentWindow", AppPos, AppSize, function()
+		--ui.drawLine(LeftStickMiddle-vec2(100,0),LeftStickMiddle+vec2(100,0),rgbm.colors.black,3)
+		--ui.drawLine(LeftStickMiddle-vec2(0,100),LeftStickMiddle+vec2(0,100),rgbm.colors.black,3)
+
 		ui.drawRectFilled(0, AppSize, rgbm(0, 0, 0, 0.2), 15, ui.CornerFlags.All)
 		--Break
 		ui.pathArcTo(LeftStickMiddle, TriggerRadius, TriggerStart, TriggerEnd, TriggerSegments)
@@ -121,5 +160,64 @@ function ControllerInput()
 		--RightStick
 		ui.drawCircle(RightStickMiddle, StickBoundsRadius, rgbm(1, 1, 1, 0.8), StickBoundSegments, 2)
 		ui.drawCircleFilled(RightStickMiddle + RightStick, StickRadius, rgbm(1, 1, 1, 0.8), StickSegments)
+
+		--DPAD
+		ui.setCursor(Dpad_pos_up)
+		if ac.isGamepadButtonPressed(Controller,ac.GamepadButton.DPadUp) then
+			ui.icon(DPAD_UP_ON,DPAD_Size,DPAD_color)
+		else
+			ui.icon(DPAD_UP_OFF,DPAD_Size,DPAD_color)
+		end
+
+		ui.setCursor(Dpad_pos_right)
+		if ac.isGamepadButtonPressed(Controller,ac.GamepadButton.DPadRight) then
+			ui.icon(DPAD_RIGHT_ON,DPAD_Size,DPAD_color)
+		else
+			ui.icon(DPAD_RIGHT_OFF,DPAD_Size,DPAD_color)
+		end
+
+		ui.setCursor(Dpad_pos_down)
+		if ac.isGamepadButtonPressed(Controller,ac.GamepadButton.DPadDown) then
+			ui.icon(DPAD_DOWN_ON,DPAD_Size,DPAD_color)
+		else
+			ui.icon(DPAD_DOWN_OFF,DPAD_Size,DPAD_color)
+		end
+
+		ui.setCursor(Dpad_pos_left)
+		if ac.isGamepadButtonPressed(Controller,ac.GamepadButton.DPadLeft) then
+			ui.icon(DPAD_LEFT_ON,DPAD_Size,DPAD_color)
+		else
+			ui.icon(DPAD_LEFT_OFF,DPAD_Size,DPAD_color)
+		end
+
+		--rightstick one
+		ui.setCursor(other_pos_left)
+		if ac.isControllerGearUpPressed() then
+			ui.icon(ShiftUp_ON,DPAD_Size,other_color)
+		else
+			ui.icon(ShiftUp_OFF,DPAD_Size,other_color)
+		end
+
+		ui.setCursor(other_pos_down)
+		if ac.isControllerGearDownPressed() then
+			ui.icon(ShiftDown_ON,DPAD_Size,other_color)
+		else
+			ui.icon(ShiftDown_OFF,DPAD_Size,other_color)
+		end
+
+		ui.setCursor(other_pos_right)
+		if car.handbrake > 0 then
+			ui.icon(HandBreak_ON,DPAD_Size,other_color)
+		else
+			ui.icon(HandBreak_OFF,DPAD_Size,other_color)
+		end
+
+		ui.setCursor(other_pos_up)
+		if car.clutch < 0.9 then
+			ui.icon(Clutch_ON,DPAD_Size,other_color)
+		else
+			ui.icon(Clutch_OFF,DPAD_Size,other_color)
+		end
+		
 	end)
 end
